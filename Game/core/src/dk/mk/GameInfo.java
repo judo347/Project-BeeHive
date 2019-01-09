64268e69131d2b34c682705bb01766c1427ff8c0
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-01-09 21:55:36 +0100

    added MAX_BEES_PER_QUEEN to GameInfo + additional readability.

--- Game/core/src/dk/mk/GameInfo.java
+++ Game/core/src/dk/mk/GameInfo.java
@@ -2,15 +2,18 @@ package dk.mk;
 
 public class GameInfo {
 
+    //Technical specifications
     public static final int WINDOW_WIDTH = 1000; //Default = 1000
     public static final int WINDOW_HEIGHT = 800; //Default = 800
     public static final int FOREGROUND_FPS = 60; //Default = 60
     public static final String TITLE = "Project BeeHive";
 
+    //Overall game specifications
     public static final int SQUARE_SIZE = 10; //Size of the game squares //Default = 10
     public static final float TICK_DURATION = 0.2f; //Duration of each game tick. Lower = faster //Default = 1f (Has to be float)
 
+    //In-game specifications
     public static final int BEE_FLOWER_ALERT_DIST = 10;
-
     public static final float BEE_LIFETIME = 1f; //The max lifetime for a bee
+    public static final int MAX_BEES_PER_QUEEN = 5;
 }
